25f5906b22a3c0e8105bb38c91d0cb4421956102
wensimin <hukaizhiyu2@gmail.com>
2021-03-03 22:36:38 +0800

    oauth testing

--- app/src/main/java/com/github/wensimin/boliboli_android/MainActivity.kt
+++ app/src/main/java/com/github/wensimin/boliboli_android/MainActivity.kt
@@ -1,12 +1,7 @@
 package com.github.wensimin.boliboli_android
 
-import android.content.ContentValues
-import android.content.Context
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import android.util.Log
-import androidx.activity.result.contract.ActivityResultContract
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -14,28 +9,37 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.github.wensimin.boliboli_android.manager.TokenManager
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import net.openid.appauth.*
-import net.openid.appauth.connectivity.ConnectionBuilder
 import java.net.HttpURLConnection
 import java.net.URL
+import java.util.function.Consumer
 
+private const val TAG: String = "MAIN ACTIVITY"
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var tokenManager: TokenManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         tokenManager = TokenManager(this, this)
+        tokenManager.login(
+            Runnable {
+                Log.d(TAG, "login ok")
+                tokenManager.testRequest(
+                    success = Consumer { s -> Log.d(TAG, "onResume: $s") },
+                    error = Consumer {
+                        //TODO move to login activity
+                    })
+            })
         setContentView(R.layout.activity_main)
-        tokenManager.login()
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
-                setOf(
-                        R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
-                )
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+            )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
--- app/src/main/java/com/github/wensimin/boliboli_android/manager/TokenManager.kt
+++ app/src/main/java/com/github/wensimin/boliboli_android/manager/TokenManager.kt
@@ -3,76 +3,145 @@ package com.github.wensimin.boliboli_android.manager
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import android.util.Log
 import androidx.activity.result.ActivityResultCaller
 import androidx.activity.result.contract.ActivityResultContract
 import net.openid.appauth.*
 import net.openid.appauth.connectivity.ConnectionBuilder
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpMethod
+import org.springframework.http.converter.StringHttpMessageConverter
+import org.springframework.web.client.RestTemplate
 import java.net.HttpURLConnection
 import java.net.URL
+import java.nio.charset.Charset
+import java.util.function.Consumer
+
 
 //TODO config
 const val OAUTH_SERVER: String = "http://192.168.0.201:81/authorization"
+const val RESOURCE_SERVER: String = "http://192.168.0.201:8080/boliboli-api"
+
+/**
+ * 认证状态
+ */
+private val authState: AuthState = AuthState()
+private const val TAG: String = "TOKEN MANAGER"
 
 class TokenManager(
-        private val context: Context,
-        private val caller: ActivityResultCaller
+    private val context: Context,
+    private val caller: ActivityResultCaller
 ) {
-    /**
-     * 认证状态
-     */
-    private var authState: AuthState = AuthState()
+
 
     // Client secret
     private val clientAuthentication: ClientSecretBasic = ClientSecretBasic("androidSecret")
-    private val serviceConfiguration: AuthorizationServiceConfiguration = AuthorizationServiceConfiguration(
+    private val serviceConfiguration: AuthorizationServiceConfiguration =
+        AuthorizationServiceConfiguration(
             Uri.parse("$OAUTH_SERVER/oauth2/authorize"),  // Authorization endpoint
             Uri.parse("$OAUTH_SERVER/oauth2/token") // Token endpoint
-    )
+        )
     private var authRequest: AuthorizationRequest = AuthorizationRequest.Builder(
-            serviceConfiguration,
-            "boliboli-android",  // Client ID
-            ResponseTypeValues.CODE,
-            Uri.parse("boliboli://oauth2") // Redirect URI
-    ).build()
+        serviceConfiguration,
+        "boliboli-android",  // Client ID
+        ResponseTypeValues.CODE,
+        Uri.parse("boliboli://oauth2") // Redirect URI
+    ).setScope("profile openid") //scope
+        .build()
     private var service: AuthorizationService = AuthorizationService(context)
 
-    fun login() {
 
-        caller.registerForActivityResult(object : ActivityResultContract<AuthorizationRequest, Intent?>() {
-            override fun createIntent(
-                    context: Context,
-                    input: AuthorizationRequest
-            ): Intent {
+    /**
+     * 进行oauth2 login
+     */
+    fun login(
+        success: Runnable, error: Consumer<AuthorizationException?> = Consumer { e ->
+            Log.d(TAG, "oauth2 login error" + e?.errorDescription)
+        }
+    ) {
+        caller.registerForActivityResult(object :
+            ActivityResultContract<AuthorizationRequest, Intent?>() {
+            override fun createIntent(context: Context, input: AuthorizationRequest): Intent {
                 return service.getAuthorizationRequestIntent(authRequest)
             }
 
             override fun parseResult(resultCode: Int, intent: Intent?): Intent? {
                 return intent
             }
-        })
-        { result ->
+        }) { result ->
             if (result == null) {
+                error.accept(null)
                 return@registerForActivityResult
             }
             val authResponse = AuthorizationResponse.fromIntent(result)
             val authException = AuthorizationException.fromIntent(result)
             authState.update(authResponse, authException)
-            if (authException != null) {
-                retrieveTokens(authResponse!!)
+            if (authResponse != null) {
+                retrieveTokens(authResponse, success, error)
+            } else {
+                error.accept(authException)
             }
         }.launch(authRequest)
     }
 
-    private fun retrieveTokens(response: AuthorizationResponse) {
-        val tokenRequest: TokenRequest = response!!.createTokenExchangeRequest()
+    /**
+     * 使用code请求token并且save至authState
+     */
+    private fun retrieveTokens(
+        response: AuthorizationResponse,
+        success: Runnable,
+        error: Consumer<AuthorizationException?>
+    ) {
+        val tokenRequest: TokenRequest = response.createTokenExchangeRequest()
         // test config 无视https
-        val config = AppAuthConfiguration.Builder().setConnectionBuilder(TestConnectionBuilder()).build()
+        val config =
+            AppAuthConfiguration.Builder().setConnectionBuilder(TestConnectionBuilder()).build()
         val service = AuthorizationService(context, config)
-        service.performTokenRequest(tokenRequest, clientAuthentication
+        service.performTokenRequest(
+            tokenRequest, clientAuthentication
         ) { tokenResponse, tokenException ->
+            //save token
             authState.update(tokenResponse, tokenException)
-            // Handle token response error here
-//            persistAuthState(mAuthState)
+            if (tokenException != null) {
+                error.accept(tokenException)
+            } else {
+                success.run()
+            }
+        }
+    }
+
+    /**
+     * 测试用请求
+     */
+    fun testRequest(
+        success: Consumer<String>, error: Consumer<AuthorizationException?> = Consumer { e ->
+            Log.d(TAG, "oauth2 login error" + e?.errorDescription)
+        }
+    ) {
+        authState.performActionWithFreshTokens(
+            service,
+            clientAuthentication
+        ) { accessToken, _, ex ->
+            if (ex != null) {
+                error.accept(ex)
+            }
+            val restTemplate = RestTemplate()
+            restTemplate.messageConverters.add(StringHttpMessageConverter(Charset.defaultCharset()))
+            val headers = HttpHeaders()
+            headers["Authorization"] = "Bearer $accessToken"
+            val entity = HttpEntity<String>(headers)
+            Thread {
+                val response = restTemplate.exchange(
+                    "$RESOURCE_SERVER/public/test",
+                    HttpMethod.GET,
+                    entity,
+                    String::class.java
+                )
+                Log.d(TAG, response.toString())
+                success.accept(response.toString())
+            }.start()
+
         }
     }
 
