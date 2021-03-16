fad75fbe1f569313aa747d2f0aaf55d79453f0f0
wensimin <hukaizhiyu2@gmail.com>
2021-03-16 16:19:58 +0800

    view model转移至activity

--- app/src/main/java/com/github/wensimin/boliboli_android/MainActivity.kt
+++ app/src/main/java/com/github/wensimin/boliboli_android/MainActivity.kt
@@ -2,6 +2,7 @@ package com.github.wensimin.boliboli_android
 
 import android.os.Bundle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.github.wensimin.boliboli_android.ui.dashboard.DashboardViewModel
@@ -15,7 +16,7 @@ class MainActivity : QMUIFragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        mode = mode ?: DashboardViewModel(application)
+        mode = ViewModelProvider(this).get(DashboardViewModel::class.java)
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
         val navController = findNavController(R.id.nav_host_fragment)
         navView.setupWithNavController(navController)
