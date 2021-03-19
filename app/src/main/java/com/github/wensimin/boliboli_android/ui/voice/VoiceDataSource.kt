2a0cd2456fae44403d781bf65a07a782f3981474
wensimin <hukaizhiyu2@gmail.com>
2021-03-19 19:35:52 +0800

    add page

---
+++ app/src/main/java/com/github/wensimin/boliboli_android/ui/voice/VoiceDataSource.kt
@@ -0,0 +1,19 @@
+package com.github.wensimin.boliboli_android.ui.voice
+
+import androidx.paging.PageKeyedDataSource
+import com.github.wensimin.boliboli_android.rest.dto.Voice
+
+class VoiceDataSource : PageKeyedDataSource<Int, Voice>() {
+
+    override fun loadInitial(params: LoadInitialParams<Int>, callback: LoadInitialCallback<Int, Voice>) {
+        TODO("Not yet implemented")
+    }
+
+    override fun loadBefore(params: LoadParams<Int>, callback: LoadCallback<Int, Voice>) {
+        TODO("Not yet implemented")
+    }
+
+    override fun loadAfter(params: LoadParams<Int>, callback: LoadCallback<Int, Voice>) {
+        TODO("Not yet implemented")
+    }
+}
